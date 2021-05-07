d8bb1854487c724f809a3a2bec6d0c56cba50b49
IvanSerov1989 <56550918+IvanSerov1989@users.noreply.github.com>
2021-05-07 03:09:28 +0300

    Delete

--- newsportal/admin/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require_once '../inc/database.php';
-include_once("modelAdmin/modelAdmin.php");
-include_once("controllerAdmin/controllerAdmin.php");
-
-include ('routeAdmin/routingAdmin.php');//!!!!
-
-echo $response;
